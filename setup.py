ceb66912222c43c2385f67f85adb94d91e88ed65
DevSart <matheus.sartor@poli.ufrj.br>
2022-01-13 14:20:56 -0300

    "first commit"

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a repo for learning purposes, MLOps is the future!',
+    author='matheus.sartor',
+    license='MIT',
+)
---
+++ src/app/api.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 11 14:43:49 2022
+
+@author: matheus.sartor
+"""
+import os
+from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+import pickle
+
+colunas = ['tamanho','ano','frente']
+modelo = pickle.load(open('../../models/modelo.sav','rb'))
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return "Bom dia MLMundo!"
+
+@app.route('/feel/<phrase>')
+@basic_auth.required
+def feel(phrase):
+    tb = TextBlob(phrase)
+    polarity = tb.sentiment.polarity
+    return f'polaridade: {polarity}'
+
+@app.route('/cotacao',methods=['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return f'Preço: R${preco[0]}'
+app.run(debug=True, host='0.0.0.0')
